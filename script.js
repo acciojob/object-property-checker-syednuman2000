ef2b5fff141dcbe33a0e810522900f8945b29424
Syed Numan <111847221+syednuman2000@users.noreply.github.com>
2023-04-12 11:09:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	for(let i in sampleObject){
+		if(i===key){
+			return true;
+		}
+	}
 }
 
 // Do not change the code below
